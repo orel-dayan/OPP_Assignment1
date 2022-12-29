febd5647646ac5dc843c068a5e4582d12155a96a
orel <orel.dayan2@gmail.com>
2022-12-29 02:18:10 +0200

    fix

--- src/test/java/TestMembers.java
+++ src/test/java/TestMembers.java
@@ -1,23 +1,30 @@
 import observer.ConcreteMember;
 import observer.GroupAdmin;
 import observer.Member;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMembers {
+    GroupAdmin groupAdmin;
+    @BeforeEach
+    void setUp() {
+        this.groupAdmin = new GroupAdmin();
+    }
+
+
     @Test
     void register() {
         ConcreteMember c1 = new ConcreteMember("orel");
         ConcreteMember c2 = new ConcreteMember("roy");
         ConcreteMember c3 = new ConcreteMember("lili");
         ConcreteMember c4 = new ConcreteMember("shir");
-        GroupAdmin groupAdmin = new GroupAdmin();
         groupAdmin.register(c1);
         groupAdmin.register(c2);
         groupAdmin.register(c3);
         groupAdmin.register(c4);
         groupAdmin.register(c4); // msg about dup
-        ArrayList<Member> list = new ArrayList<Member>();
+        ArrayList<Member> list = new ArrayList<>();
         list.add(c1);
         list.add(c2);
         list.add(c3);
@@ -33,30 +40,95 @@ public class TestMembers {
         ConcreteMember member2 = new ConcreteMember("B");
         ConcreteMember member3 = new ConcreteMember("C");
         ConcreteMember member4 = new ConcreteMember("D");
-        GroupAdmin ga = new GroupAdmin();
-        ga.register(member1);
-        ga.register(member2);
-        ga.register(member3);
-        ga.unregister(member1);
-        ga.unregister(member2);
-        ga.unregister(member3);
-        ga.unregister(member4);
-        assertEquals(ga.getMembers().size(), 0);
+        GroupAdmin groupAdmin = new GroupAdmin();
+        groupAdmin.register(member1);
+        groupAdmin.register(member2);
+        groupAdmin.register(member3);
+        groupAdmin.unregister(member1);
+        groupAdmin.unregister(member2);
+        groupAdmin.unregister(member3);
+        groupAdmin.unregister(member4);
+        assertEquals(groupAdmin.getMembers().size(), 0);
     }
     @Test
     void notifyMembers() {
-        GroupAdmin gad = new GroupAdmin();
+
         ConcreteMember c1 = new ConcreteMember("orel");
         ConcreteMember c2 = new ConcreteMember("roy");
         ConcreteMember c3 = new ConcreteMember("lili");
-        gad.register(c1);
-        gad.register(c2);
-        gad.register(c3);
-        gad.append("obj");
-        gad.notifyAllMembers();
-        assertEquals(c1.getData(), gad.toString());
-        gad.undo();
-        gad.notifyAllMembers();
-        assertEquals(c2.getData(), gad.toString());
+        groupAdmin.register(c1);
+        groupAdmin.register(c2);
+        groupAdmin.register(c3);
+        groupAdmin.append("obj");
+        groupAdmin.notifyAllMembers();
+        assertEquals(c1.getData(), groupAdmin.toString());
+        groupAdmin.undo();
+        groupAdmin.notifyAllMembers();
+        assertEquals(c2.getData(), groupAdmin.toString());
     }
-}
+    @Test
+    void insert() {
+
+        ConcreteMember member = new ConcreteMember("A");
+        groupAdmin.register(member);
+        groupAdmin.insert(0, "test");
+        assertEquals("test", groupAdmin.toString()); // check if update
+        groupAdmin.insert(4, " insert");
+        assertEquals("test insert", groupAdmin.toString()); //check if update
+        groupAdmin.insert(-2, "!"); // print error and see if update
+
+    }
+    @Test
+    void append() {
+
+        ConcreteMember member = new ConcreteMember("A");
+        groupAdmin.register(member);
+        groupAdmin.append("TEST");
+        assertEquals("TEST", groupAdmin.toString());
+        ConcreteMember observer2 = new ConcreteMember("B");  // Create another member and register him.
+        groupAdmin.register(observer2);
+        groupAdmin.append("!");  //  append " !" and check if updated.
+        assertEquals("TEST!", groupAdmin.toString());
+    }
+
+    @Test
+    void delete() {
+        ConcreteMember concreteMember = new ConcreteMember("A");
+        groupAdmin.register(concreteMember);
+        groupAdmin.delete(0,5);
+        assertEquals("", groupAdmin.toString());
+        groupAdmin.append("Test");  // append and check to see if updated.
+        groupAdmin.delete(0,3);
+        assertEquals("t", groupAdmin.toString());
+        groupAdmin.delete(0, -9); // out of bounds
+    }
+
+    @Test
+    void undo() {
+     ConcreteMember concreteMember = new ConcreteMember("A");
+     ConcreteMember member = new ConcreteMember("B");
+     groupAdmin.register(member);
+     groupAdmin.register(concreteMember);
+     groupAdmin.append("TEST");
+     groupAdmin.append("test");
+     groupAdmin.undo();
+     assertEquals(groupAdmin.toString(),concreteMember.getData(),member.getData());
+     groupAdmin.undo();
+     assertEquals(groupAdmin.toString(),concreteMember.getData(), concreteMember.getData());
+
+    }
+    @Test
+    void update() {
+    ConcreteMember member = new ConcreteMember("A");
+    ConcreteMember member1 = new ConcreteMember("B");
+    groupAdmin.register(member);
+    groupAdmin.register(member1);
+    groupAdmin.append("hello");
+    assertEquals("hello",groupAdmin.toString());
+    groupAdmin.unregister(member1);
+    groupAdmin.undo();
+    assertEquals("", groupAdmin.toString());
+    }
+
+    }
+
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -12,141 +12,136 @@ public class Tests {
 
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
     // stub method to check external dependencies compatibility
-    GroupAdmin groupAdmin = new GroupAdmin();
-    GroupAdmin groupAdmin1 = new GroupAdmin();
-    GroupAdmin groupAdmin2=new GroupAdmin();
-    String s1 = "Alice";
-    String s2 = "Bob";
-    ConcreteMember member1 = new ConcreteMember(s1);
-    ConcreteMember member2 = new ConcreteMember(s2);
+    GroupAdmin tester = new GroupAdmin();
 
-    // stub method to check external dependencies compatibility
     @BeforeEach
     void setUp() {
-        groupAdmin.register(member1);
-        groupAdmin.register(member2);
+        this.tester = new GroupAdmin();
     }
-    @AfterEach
-    void testMemory(){
-        logger.info(JvmUtilities::jvmInfo);
-    }
-    // check register and unregister size
-    @Test
-    public void testMemoryRegisterChanges(){
 
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.unregister(member1);
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.unregister(member2);
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-    }
-    @Test
-    public void test() {
-        GroupAdmin tester = new GroupAdmin();
-        ConcreteMember m1 = new ConcreteMember("A");
-        ConcreteMember m2 = new ConcreteMember("B");
-        ConcreteMember m3 = new ConcreteMember("C");
-
-        System.out.println("Testing footprint and  total size");
-
-        logger.info(() -> JvmUtilities.objectFootprint(tester, m1, m2, m3));
-        logger.info(() -> JvmUtilities.objectTotalSize(tester, m1, m2, m3));
-        System.out.println();
-        System.out.println("Registering members: ");
-
-        tester.register(m1);  //added member
-        tester.register(m2);  //added member
-        tester.register(m3);  //added member
-
-        logger.info(() -> JvmUtilities.objectFootprint(tester));
-        logger.info(() -> JvmUtilities.objectTotalSize(tester));
-
-        tester.append("1");
-        System.out.println();
-
-        System.out.println("let's see what happen to the memory after we appended the string 123 ");
-        logger.info(() -> JvmUtilities.objectFootprint(tester));
-        logger.info(() -> JvmUtilities.objectTotalSize(tester));
-        System.out.println();
-
-        System.out.println("Now let's unregister a members and see what's happen to the memory aloccate: ");
-        tester.unregister(m2);
-        System.out.println();
-        logger.info(() -> JvmUtilities.objectFootprint(tester));
-        logger.info(() -> JvmUtilities.objectTotalSize(tester));
-        tester.unregister(m3);
-        System.out.println();
-        logger.info(() -> JvmUtilities.memoryStats(tester)); // return the objectfootprint & objectTotalSize together
-
-        System.out.println("Printing info: ");
-        logger.info(() -> JvmUtilities.jvmInfo());
-        System.out.println();
-
-        System.out.println("insert an object and see the memory state: ");
-        tester.insert(1, "avi");
-
-        System.out.println();
-    }
-    @Test
-    public void testMemoryStrings(){
-
-        groupAdmin.append("Hello");
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.append(" world!");
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.delete(0,5);
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.insert(0,"Hello");
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-        groupAdmin.undo();
-        logger.info(() -> JvmUtilities.objectFootprint(groupAdmin));
-    }
+    // stub method to check external dependencies compatibility
 
     @Test
-    public void TestGroupAdmin() {
-        logger.info(() -> JvmUtilities.objectTotalSize(groupAdmin1));
-        groupAdmin1.append("Great");
-        logger.info(() -> JvmUtilities.objectTotalSize(groupAdmin1));
+     void testFooPrintAndTotalSize() {
+
         ConcreteMember c1 = new ConcreteMember("A");
         ConcreteMember c2 = new ConcreteMember("B");
         ConcreteMember c3 = new ConcreteMember("C");
-        ConcreteMember c4 = new ConcreteMember("D");
-        groupAdmin1.register(c1);
-        groupAdmin1.register(c2);
-        groupAdmin1.register(c3);
-        groupAdmin1.register(c4);
-        groupAdmin1.register(c4); // return  message about duplicate
-        groupAdmin1.append("!!!");
-        //Checking if they have the same size
-        logger.info(() -> JvmUtilities.objectTotalSize(c1));
-        logger.info(() -> JvmUtilities.objectTotalSize(c2));
-        assertEquals(JvmUtilities.objectTotalSize(c1), JvmUtilities.objectTotalSize(c2));
-        groupAdmin1.undo();
-        assertEquals("Great", c1.getData());
-        groupAdmin1.unregister(c4);
-        groupAdmin1.delete(0, 1);
+        System.out.println("Testing footprint and  total size");
+        logger.info(() -> JvmUtilities.objectFootprint(tester, c1, c2, c3));
+        logger.info(() -> JvmUtilities.objectTotalSize(tester, c1, c2, c3));
+        System.out.println();
+        tester.register(c1);
+        tester.register(c2);
+        tester.register(c3);
+        logger.info(() -> JvmUtilities.objectFootprint(tester));
+        logger.info(() -> JvmUtilities.objectTotalSize(tester));
+
+        tester.append("hello"); // append string
+        System.out.println();
+
+        System.out.println("what happen to the memory after we appended the string hello ");
+        logger.info(() -> JvmUtilities.objectFootprint(tester));
+        logger.info(() -> JvmUtilities.objectTotalSize(tester));
+        System.out.println();
+
+        System.out.println("unregister a members, memory allocate: ");
+        tester.unregister(c2);
+        System.out.println();
+        logger.info(() -> JvmUtilities.objectFootprint(tester));
+        logger.info(() -> JvmUtilities.objectTotalSize(tester));
+        tester.unregister(c3);
+        System.out.println();
+        logger.info(() -> JvmUtilities.memoryStats(tester)); // return the object-footprint and objectTotalSize
+
+        System.out.println("printing info: ");
+        logger.info(JvmUtilities::jvmInfo);
+        System.out.println();
+
+    }
+    @Test
+    void checkSize() {
+
+        // size of admin
+        logger.info(()->JvmUtilities.objectTotalSize(tester));
+
+        ConcreteMember c1 = new ConcreteMember("A");
+        ConcreteMember c2 = new ConcreteMember("B");
+        ConcreteMember c3 = new ConcreteMember("C");
+
+        // register the members
+        tester.register(c1);
+        tester.register(c2);
+        tester.register(c2); // msg
+
+
+
+        System.out.println("before insert");
+        // append a string
+        tester.append("grea");
+        System.out.println();
+
+        // insert a string at index 4
+        tester.insert(4, "t");
+        logger.info(()->"total memory after insert:");
+        logger.info(()->JvmUtilities.objectTotalSize(c1));
+        System.out.println();
+        logger.info(()->JvmUtilities.objectTotalSize(c2));
+        // check if they have the same size
+        assertEquals(JvmUtilities.objectTotalSize(c1),JvmUtilities.objectTotalSize(c2));
+        System.out.println();
+
+
+        // append a string
+        tester.append(" day");
+        logger.info(()->"total memory after append:");
+        logger.info(()->JvmUtilities.objectTotalSize(c1));
+        System.out.println();
+        logger.info(()->JvmUtilities.objectTotalSize(c2));
+        // check if they both have the same size after append
+        assertEquals(JvmUtilities.objectTotalSize(c1),JvmUtilities.objectTotalSize(c2));
+
+
+        // undo the delete action
+        tester.undo();
+        System.out.println();
+        assertEquals("great",c1.getData());
+        logger.info(()->"total memory after undo:");
+        logger.info(()->JvmUtilities.objectTotalSize(c1));
+
+        // delete a range of characters
+        tester.delete(0,1);
+        System.out.println();
         assertEquals("reat", c1.getData());
-        assertEquals(c2.getData(), c3.getData());
-        logger.info(() -> (" Size before unregister is:"));
-        logger.info(() -> JvmUtilities.objectTotalSize(groupAdmin1));
-        groupAdmin1.unregister(c3);
-        logger.info(() -> ("Size  after unregister:"));
-        logger.info(() -> JvmUtilities.objectTotalSize(groupAdmin1));
+        assertEquals(c2.getData(),c2.getData());
+        logger.info(()->"total memory after delete:");
+        logger.info(()->JvmUtilities.objectTotalSize(c1));
+
+        // register a member
+        tester.register(c3);
+        logger.info(()->"total memory after register a member:");
+        logger.info(()->JvmUtilities.objectTotalSize(tester));
+
+
+        // unregister a member
+        tester.unregister(c3);
+        logger.info(()->"total memory after unregister a member:");
+        logger.info(()->JvmUtilities.objectTotalSize(tester));
+
     }
 
     @Test
-    public void TestConcreteMember() {
-
+     void TestConcrete() {
         ConcreteMember m = new ConcreteMember("A");
         logger.info(() -> JvmUtilities.objectTotalSize(m));
-        groupAdmin2.register(m);
-        groupAdmin2.append("to sleep or not to sleep?");
+        tester.register(m);
+        tester.append("to sleep or not to sleep?");
         assertEquals("to sleep or not to sleep?",m.getData());
-        groupAdmin2.append(" to sleep");
+        tester.append(" to sleep");
         assertEquals("to sleep or not to sleep? to sleep", m.getData());
         logger.info(() -> JvmUtilities.objectTotalSize(m));
-        groupAdmin2.append("!");
-        groupAdmin2.unregister(m);
+        tester.append("!");
+        tester.unregister(m);
         UndoableStringBuilder usb = new UndoableStringBuilder();
         usb.append("hello");
         m.update(usb);
